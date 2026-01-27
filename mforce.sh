feb87bbd27a029c780ac99e6e1658a965c367b09
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-01-27 23:31:09 +0100

    Update mforce.sh

--- mforce.sh
+++ mforce.sh
@@ -1,1 +1,16 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+source core/banner.sh
+
+banner
+
+echo "[1] WiFi OSINT"
+echo "[0] Exit"
+echo
+read -p "mForce >> " choice
+
+case $choice in
+  1) bash modules/wifi_osint.sh ;;
+  0) exit ;;
+  *) echo "Invalid option" ;;
+esac
