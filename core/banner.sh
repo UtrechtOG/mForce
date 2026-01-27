3be6ce1b58ebc7867a097459d3a99f5aae96a989
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-01-27 23:29:05 +0100

    Update banner.sh

--- core/banner.sh
+++ core/banner.sh
@@ -1,1 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+source core/colors.sh
+
+banner() {
+  clear
+  echo -e "${CYAN}"
+  echo "███╗     ███╗██████╗  ██████╗ █████╗     ██████╗  ███████╗"
+  echo "████╗  ████║██╔════██╔═   ██╔█══  █╗   ██╔════╝ ██╔════╝"
+  echo "██╔████╔██║█████╗  ██║     ██║█  ██║    ██║          █████╗  "
+  echo "██║╚██╔╝██║██╔══╝  ██║     ██║█  ██║     ██║         ██╔══╝  "
+  echo "██║ ╚═╝  ██  ██╗        ╚██████╗█     █╔     ██████╗  ███████╗"
+  echo "╚═╝     ╚═╝╚══════╝ ╚═════╝ ╚═════╝  ╚═════╝╚══════╝"
+  echo -e "${NC}"
+  echo -e "${YELLOW}MultiForce Security Framework for Termux${NC}"
+  echo
+}
