88163a8f8ed14ae150ac081d5377883aa773741b
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-01-28 02:41:28 +0100

    Update mforce.sh

--- mforce.sh
+++ mforce.sh
@@ -3,14 +3,24 @@
 source core/banner.sh
 banner
 
-echo -e "${ACCENT}[1]${NC} WiFi OSINT Scanner"
+echo -e "${ACCENT}[1]${NC} WiFi OSINT"
+echo -e "${ACCENT}[2]${NC} Access Point Fingerprinting"
 echo -e "${ACCENT}[0]${NC} Exit"
 echo
 
 read -p "mForce ▸ " choice
 
 case $choice in
-  1) bash modules/wifi_osint.sh ;;
-  0) exit ;;
-  *) echo "[!] Invalid option" ;;
+  1)
+    bash modules/wifi_osint.sh
+    ;;
+  2)
+    bash modules/ap_fingerprint.sh
+    ;;
+  0)
+    exit
+    ;;
+  *)
+    echo "[!] Invalid option"
+    ;;
 esac
