62a86d52dd8aa49789eefcd653737ea76cf6cba3
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-01-28 02:02:05 +0100

    Update mforce.sh

--- mforce.sh
+++ mforce.sh
@@ -1,16 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 source core/banner.sh
-
 banner
 
-echo "[1] WiFi OSINT"
-echo "[0] Exit"
+echo -e "${CYAN}[1]${NC} WiFi OSINT"
+echo -e "${CYAN}[0]${NC} Exit"
 echo
+
 read -p "mForce >> " choice
 
 case $choice in
   1) bash modules/wifi_osint.sh ;;
   0) exit ;;
-  *) echo "Invalid option" ;;
+  *) echo "[!] Invalid option" ;;
 esac
