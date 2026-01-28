e22af42c981c688e5d1fc3d05ea8f6f03f2e0414
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-01-28 10:50:27 +0100

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,1 +1,15 @@
+function showRegister() {
+  document.getElementById("login-form").classList.add("hidden");
+  document.getElementById("register-form").classList.remove("hidden");
+}
 
+function showLogin() {
+  document.getElementById("register-form").classList.add("hidden");
+  document.getElementById("login-form").classList.remove("hidden");
+}
+
+function copyToken() {
+  const token = document.querySelector(".token-box code").innerText;
+  navigator.clipboard.writeText(token);
+  alert("Token copied to clipboard");
+}
